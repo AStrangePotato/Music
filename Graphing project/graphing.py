62dee00af71f7fc2d14860b5079d4c3cf46652f3
blackholestar <100437276+blackholestar@users.noreply.github.com>
2022-02-25 09:40:39 -0800

    New version with graphing options

--- Graphing project/graphing.py
+++ Graphing project/graphing.py
@@ -3,48 +3,73 @@ import matplotlib.pyplot as plt
 import numpy as np
 from playsound import playsound
 
+import math
+
 notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4"]
 # Create the vectors X and Y
 
-quantity = 10
+quantity = 250
+
+def linear():
+    return list(range(quantity))
+def quadratic():
+    a = list(range(quantity))
+    for i in range(len(a)):
+        a[i] = a[i] ** 2
+    '''for i in range(len(a)):
+        a[i] = a[i] % 40'''
+    return a
+def triangular():
+    a = []
+    for i in range(quantity):
+        a.append(i * (i+1) / 2)
+    return a
+def sine():
+    a = []
+    for i in range(quantity):
+        a.append(10 *math.sin(0.2 * i))
+    return a
+def fibonacci():
+    a = [1,1]
+    for i in range(quantity - 2):
+        a.append(a[i-1]+a[i-2])
+    return a
+pitch_formula = int(input("Enter pitch formula\n1: linear, 2: quadratic, 3: triangular, 4: sine, 5: fibonacci\n"))
+tempo_formula = int(input("Enter tempo formula\n1: linear, 2: quadratic, 3: triangular, 4: sine, 5: fibonacci\n"))
+
+if pitch_formula == 1: #linear
+    x_pitch = linear()
+    y_pitch = linear()
+elif pitch_formula == 2: #quadratic
+    x_pitch = linear()
+    y_pitch = quadratic()
+elif pitch_formula == 3: #triangular
+    x_pitch = linear()
+    y_pitch = triangular()
+elif pitch_formula == 4: #sine
+    x_pitch = linear()
+    y_pitch = sine()
+elif pitch_formula == 5: #fibonacci
+    x_pitch = linear()
+    y_pitch = fibonacci()
+    
+if tempo_formula == 1: #linear
+    x_tempo = linear()
+    y_tempo = linear()
+elif tempo_formula == 2: #quadratic
+    x_tempo = linear()
+    y_tempo = quadratic()
+elif tempo_formula == 3: #triangular
+    x_tempo = linear()
+    y_tempo = triangular()
+elif tempo_formula == 4: #sine
+    x_tempo = linear()
+    y_tempo = sine()
+elif tempo_formula == 5: #fibonacci
+    x_tempo = linear()
+    y_tempo = fibonacci()
 
 
-imSoLost = []
-
-x_pitch = np.array(range(quantity))
-temp = input("Enter a formula for pitch graphing: *USE 'x'* \n")
-if temp not None:
-    y_pitch_formula = temp
-else:
-    y_pitch_formula = x_pitch * (x_pitch+1) / 2
-
-y_pitch = y_pitch_formula
-
-
-
-x_tempo = np.array(range(quantity))
-
-x_pitch_formula = x_tempo ** 2
-
-y_tempo = x_pitch_formula
-
-
-for x in range(quantity):
-    imSoLost.append(x * (x+1) / 2)
-
-print(imSoLost)   
-
-
-
-'''
-round to nearest note on piano
-play that note
-1 = c
-2= c#
-etc
-make list with y values for specified x increment
-
-'''
 
 # Add a title
 plt.title('Graphing Music Project')
@@ -53,8 +78,8 @@ plt.title('Graphing Music Project')
 plt.grid(alpha=.4,linestyle='--')
 
 # Create the plot
-plt.plot(x_pitch, y_pitch, label='Pitch: x * (x+1) / 2')
-plt.plot(x_tempo, y_tempo, label='Tempo: x ** 2')
+plt.plot(x_pitch, y_pitch, label='Pitch')
+plt.plot(x_tempo, y_tempo, label='Tempo')
 
 plt.xlabel('x axis')
 plt.ylabel('y axis' )
@@ -65,6 +90,4 @@ plt.legend()
 
 # Show the plot
 plt.show()
-
-
 
